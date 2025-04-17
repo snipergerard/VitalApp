8256605651b5bf349c3f969f8a7d035c3c0c131f
snipergerard <gerardoloquillo10@gmail.com>
2025-04-16 20:18:37 -0600

    Add files via upload

---
+++ doctor/doctor_view.py
@@ -0,0 +1,7 @@
+from tkinter import * 
+def abrir_doctor():
+   
+    ventana = Tk()
+    ventana.title("Panel Medico")
+    Label(ventana, text="Bienvenido, Medico", font=("Arial", 16)).pack(pady=50)
+    ventana.mainloop()
