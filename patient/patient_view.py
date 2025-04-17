b2b07cf8f9c50c1148fb78275c0407d036e1f49a
snipergerard <gerardoloquillo10@gmail.com>
2025-04-16 20:19:16 -0600

    Add files via upload

---
+++ patient/patient_view.py
@@ -0,0 +1,6 @@
+from tkinter import *
+def abrir_patient():
+    ventana = Tk()
+    ventana.title("Panel Paciente")
+    Label(ventana, text="Bienvenido, Paciente", font=("Arial", 16)).pack(pady=50)
+    ventana.mainloop()
